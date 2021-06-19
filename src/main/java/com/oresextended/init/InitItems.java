e089ff22faccac8ed39b4a08124c19414e837551
Jeffrey Moon <moon.jeffreyw@gmail.com>
2021-06-19 12:54:14 -0400

    cobalt tools complete

--- src/main/java/com/oresextended/init/InitItems.java
+++ src/main/java/com/oresextended/init/InitItems.java
@@ -11,17 +11,25 @@ import net.minecraft.util.registry.Registry;
 public class InitItems {
 
     /**
-     * Cobalt Ingot
+     * Ingots
      */
     private static final Item COBALT_INGOT = new Item((new Item.Settings()).group(ItemGroup.MATERIALS));
 
     /**
-     * Cobalt Axe
+     * Cobalt Tools
      */
     private static final ToolItem COBALT_AXE = new CobaltAxeItem(CobaltToolMaterial.INSTANCE, 2, -3.0f, new Item.Settings().group(ItemGroup.TOOLS));
+    private static final ToolItem COBALT_PICKAXE = new CobaltAxeItem(CobaltToolMaterial.INSTANCE, 2, -3.0f, new Item.Settings().group(ItemGroup.TOOLS));
+    private static final ToolItem COBALT_HOE = new CobaltAxeItem(CobaltToolMaterial.INSTANCE, 2, -3.0f, new Item.Settings().group(ItemGroup.TOOLS));
+    private static final ToolItem COBALT_SHOVEL = new CobaltAxeItem(CobaltToolMaterial.INSTANCE, 2, -3.0f, new Item.Settings().group(ItemGroup.TOOLS));
+
+
 
     public static void Init() {
         Registry.register(Registry.ITEM, new Identifier(Config.NAMESPACE, "cobalt_ingot"), COBALT_INGOT);
         Registry.register(Registry.ITEM, new Identifier(Config.NAMESPACE, "cobalt_axe"), COBALT_AXE);
+        Registry.register(Registry.ITEM, new Identifier(Config.NAMESPACE, "cobalt_pickaxe"), COBALT_PICKAXE);
+        Registry.register(Registry.ITEM, new Identifier(Config.NAMESPACE, "cobalt_hoe"), COBALT_HOE);
+        Registry.register(Registry.ITEM, new Identifier(Config.NAMESPACE, "cobalt_shovel"), COBALT_SHOVEL);
     }
 }
